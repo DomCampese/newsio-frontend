7a28ae2ad04445f1d6dfac0cab166e36a52decd3
Dominic Campese <62118391+DomCampese@users.noreply.github.com>
2023-03-10 21:09:32 -0500

    Fix typo

--- newsio/src/index.js
+++ newsio/src/index.js
@@ -9,7 +9,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import NewsSearch from 'components/NewsSearch';
+import NewsSearch from 'components/SearchNews';
 import SavedNews from 'components/SavedNews';
 
 const router = createBrowserRouter([
