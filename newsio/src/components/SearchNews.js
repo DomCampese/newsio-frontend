c90d91ab213013812560a24240dad818b27f2f9f
Dominic Campese <62118391+DomCampese@users.noreply.github.com>
2023-04-02 21:39:24 -0400

    Add search bar on search page and validation

--- newsio/src/components/SearchNews.js
+++ newsio/src/components/SearchNews.js
@@ -1,9 +1,47 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { Box, Container, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
 
 const NewsSearch = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTermInvalid, setSearchTermInvalid] = useState(false);
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const letterRegex = /^[a-zA-Z\s]+$/
+    const searchTerm = String(data.get('searchTerm'));
+    if (!letterRegex.test(searchTerm)) {
+      setSearchTermInvalid(true);
+      return;
+    }
+    setSearchTermInvalid(false);
+    setSearchTerm(searchTerm);
+  }
+
   return (
-    <Typography variant='h4' textAlign='center'>News Search</Typography>
+    <Container>
+      <Typography variant='h4' textAlign='center'>News Search</Typography>
+      <Box
+        component="form"
+        onSubmit={handleUpdate}
+        sx={{ mt: 3 }}
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <TextField
+          sx={{ width: 600 }}
+          size="medium"
+          id="search-bar"
+          label="Keywords"
+          variant="outlined"
+          name="searchTerm"
+          type='text'
+          error={searchTermInvalid}
+          helperText={searchTermInvalid && 'Letters and spaces only'}
+        />
+      </Box>
+    </Container>
   )
 }
 
